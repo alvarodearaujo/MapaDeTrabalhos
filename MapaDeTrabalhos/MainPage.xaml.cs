9fa1b6f21ba243afbd130332237910dc28f92531
Álvaro de Araújo <alvarodearaujo13@gmail.com>
2015-11-23 16:30:53 -0300

    First Commit.

---
+++ MapaDeTrabalhos/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MapaDeTrabalhos
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Cadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(CadastroPage));
+        }
+
+        private void Logar_Click(object sender, RoutedEventArgs e)
+        {
+            //fazer a parada pra validar o login e direcionar a página principal.
+        }
+    }
+}
