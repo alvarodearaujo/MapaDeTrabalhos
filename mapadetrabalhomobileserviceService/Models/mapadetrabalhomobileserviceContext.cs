ef8891742e69bdd1a1ca4e5de67f582a9fa6014d
Eduardo Favarato <eduardofavarato@hotmail.com>
2015-11-30 14:07:15 -0300

    Adicionada a cor nas páginas..

--- mapadetrabalhomobileserviceService/Models/mapadetrabalhomobileserviceContext.cs
+++ mapadetrabalhomobileserviceService/Models/mapadetrabalhomobileserviceContext.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using Microsoft.WindowsAzure.Mobile.Service;
 using Microsoft.WindowsAzure.Mobile.Service.Tables;
 using mapadetrabalhomobileserviceService.DataObjects;
-using mapadetrabalhomobileserviceService.DataObjects;
 
 namespace mapadetrabalhomobileserviceService.Models
 {
